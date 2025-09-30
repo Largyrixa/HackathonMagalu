603aae9cff4b9de4c4fd7a7a6083d854f52af960
Heitor <heitovianaferreira19@gmail.com>
2025-09-30 17:55:04 -0300

    Atualizei o frontend

--- TaskRank/frontend/src/components/StatCard.tsx
+++ TaskRank/frontend/src/components/StatCard.tsx
@@ -10,15 +10,15 @@ interface StatCardProps {
 export default function StatCard({ icon, value, label }: StatCardProps){
 
     return(
-    <div className="bg-white p-4 rounded-lg border border-gray-200 flex items-center gap-4 shadow-sm">
+    <div className="bg-white p-5 rounded-lg border border-gray-200 flex items-center gap-4 shadow-sm">
       {/* Container do Ícone */}
-      <div className="bg-gray-100 p-3 rounded-lg">
+      <div className="bg-gray-100 p-4 text-gray-700 rounded-lg">
         {icon}
       </div>
 
       {/* Container do Texto */}
       <div>
-        <p className="text-2xl font-bold text-gray-800">{value}</p>
+        <p className="text-3xl font-bold text-gray-800">{value}</p>
         <p className="text-sm text-gray-500">{label}</p>
       </div>
     </div>
